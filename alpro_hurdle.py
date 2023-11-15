97ed019bd501583dfe53024ad894b2ac82d832ae
Haikal Ferdian <100331750+haikalfers@users.noreply.github.com>
2023-11-15 11:36:12 +0700

    Create alpro_hurdle.py

---
+++ alpro_hurdle.py
@@ -0,0 +1,40 @@
+"""
+Kelas : 2023E
+Prodi : D4 Manajemen Informatika
+Kelompok 4
+Anggota Kelompok
+1. Zaidan Dhiya Ulhaq (23091397152)
+2. Alya Faadhilah Putri (23091397153)
+3. Haikal Ferdian Saputra (23091397154)
+"""
+
+#Hurdle 1
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def mlampah():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+
+def jalan_kaki():
+    move()
+    turn_left()
+  
+mlampah()
+jalan_kaki()
+mlampah()
+jalan_kaki()
+mlampah()
+jalan_kaki()
+mlampah()
+jalan_kaki()
+mlampah()
+jalan_kaki()
+mlampah()
+jalan_kaki()
